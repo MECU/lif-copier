c5e702846d390f29e14dd2686aed7ce782694099
Michael Hoppes <michael.hoppes@checkr.com>
2022-05-06 17:59:24 -0600

    split into main and non; setup goreleaser

---
+++ go.mod
@@ -0,0 +1,5 @@
+module lif-copier
+
+go 1.13
+
+require github.com/fsnotify/fsnotify v1.5.4
--- lif-notifier.go
+++ lif-notifier.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"log"
-	"fmt"
-	"net/http"
-	"github.com/go-fsnotify/fsnotify"
-	"strings"
 	"bytes"
-	"mime/multipart"
+	"fmt"
 	"io/ioutil"
+	"log"
+	"mime/multipart"
+	"net/http"
 	"time"
 )
 
@@ -88,53 +86,4 @@ func Upload(url, file string, key []byte) (err error) {
 		err = fmt.Errorf("bad status: %s", res.Status)
 	}
 	return
-}
-
-func main() {
-	// Load the key
-	key, err := ioutil.ReadFile("./tractrak.key")
-	if err != nil {
-		panic(err)
-	}
-	log.Println("Got the Key")
-
-	// Setup watcher
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer watcher.Close()
-
-	log.Println("Ready to rock this meet ...")
-	done := make(chan bool)
-	go func() {
-		for {
-			select {
-			case event := <-watcher.Events:
-				if event.Op & fsnotify.Write == fsnotify.Write || event.Op & fsnotify.Create == fsnotify.Create {
-					log.Print("created/modified file:", event.Name)
-					// See if it's a .LIF file
-					// TODO: lynx.sch, .ppl, evt
-					fileExtension := event.Name[len(event.Name) - 3:]
-					if strings.ToUpper(fileExtension) == "LIF" {
-						log.Print(" ... trying to upload")
-						err = Upload("https://tractrak.com/api/meet-file", event.Name, key)
-						if err != nil {
-							log.Println(" ... upload failed: ", err)
-						} else {
-							log.Println(" ... upload success")
-						}
-					}
-				}
-			case err := <-watcher.Errors:
-				log.Println("error: ", err)
-			}
-		}
-	}()
-
-	err = watcher.Add(".")
-	if err != nil {
-		log.Fatal(err)
-	}
-	<-done
 }
